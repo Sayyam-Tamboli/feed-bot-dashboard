6aabe6dbcadd443d62e823ff8f815133a762ad0d
Sayyam Tamboli <sayyamtamboli45@gmail.com>
2026-04-26 20:44:47 +0530

    deploying the app

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const client = axios.create({ baseURL: 'http://localhost:8080/api' })
+const client = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api' })
 
 export function fetchApiKeys() {
   return client.get('/api-keys')
